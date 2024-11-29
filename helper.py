b9fd3e13ba835365f9fe5bec3ec9a1a5ab6b39fe
Pradeep Thamatam <136185028+pradeepredt@users.noreply.github.com>
2024-11-29 18:52:04 +0530

    Add files via upload

--- helper.py
+++ helper.py
@@ -38,6 +38,15 @@ def country_year_list(df):
 
     return years,country
 
+def yearwise_medal_tally(df,country):
+    temp_df = df.dropna(subset=['Medal'])
+    temp_df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Event', 'Medal'], inplace=True)
+
+    new_df = temp_df[temp_df['region'] == country]
+    final_df = new_df.groupby('Year').count()['Medal'].reset_index()
+
+    return final_df
+
 def country_event_heatmap(df,country):
     temp_df = df.dropna(subset=['Medal'])
     temp_df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Event', 'Medal'], inplace=True)
